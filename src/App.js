9c576d6d6121a68b55f4d271566fac3c87fefc7f
zheng <zhenghandeng@gmail.com>
2021-05-26 16:23:57 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 //import { Component } from 'react';
 
 import {CardList } from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
 
 class App extends Component {
@@ -43,14 +44,18 @@ class App extends Component {
     .then(users=>this.setState({monsters:users}))
   }
 
+  handleChange = (e) => {
+    this.setState({searchItem: e.target.value})
+  }
+
   render() {
     const {monsters, searchItem} = this.state
-    
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchItem.toLowerCase()))
 
     return (
     <div className='App'>
-      <input type='search' placeholder='search monster' onChange={e => this.setState({searchItem: e.target.value})}/>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox  placeholder ='search monster' handleChange={this.handleChange}/>
       <CardList monsters={filteredMonsters}> 
       </CardList>
       
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './card.styles.css'
 
 export const Card = (props) => (
-    <div  >
-    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`}/>
-    <h2 className='card-container'>{props.monster.name}</h2>
+    <div className='card-container' >
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=100x100`}/>
+    <h2 >{props.monster.name}</h2>
 
     <p> {props.monster.email}</p>
 
